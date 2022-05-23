e0ae0aac30dd0fed15fbc3b61dd373f703477d1d
hardworkingmission <shakil20131101041@gmail.com>
2022-05-23 10:36:30 +0600

    order-done

--- index.js
+++ index.js
@@ -23,17 +23,38 @@ const main=async()=>{
         availableQuantity:Number,
         price:Number
     })
+    const orderSchema=mongoose.Schema({
+        name: String,
+        email: String,
+        phone: String,
+        address: String,
+        partsId: String,
+        purchaseQuantity: Number
+
+    })
     const Parts=mongoose.model('Part',partsSchema)
+    const Order=mongoose.model('Order',orderSchema)
 
     //get all parts
     app.get('/parts',async(req,res)=>{
         const parts= await Parts.find({})
         res.send(parts)
     })
+
+    //get single item
     app.get('/partsItemById/:id',async(req,res)=>{
         const id=req.params.id
         const result= await Parts.findOne({_id:id})
         res.send(result)
+    })
+
+    //make order
+    app.post('/order',async(req,res)=>{
+        const order=req.body
+        const result=await new Order(order)
+        result.save()
+        res.send(result)
+        console.log(order)
     })
 
     console.log('Connected')
